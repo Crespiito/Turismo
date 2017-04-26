a350d6282b4e4abe5a2a51dd6ab1506ed2029f0d
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2017-04-26 18:03:53 -0500

    Login ok

--- Turismo/app/controllers/home_controller.rb
+++ Turismo/app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
+  
+  #GET /home
   def index
+  	#render :index
   end
-end
+
+  #GET /home/new
+  def new
+  	@Account = Account.new	
+  end 
+
+  #POST /home
+  def create
+  	#@Account = Account.new(Correo: params[:Account][:Correo], Contraseña: params[:Acconut][:Contraseña])
+  	#if @Account.save
+  	#	flash[:notice] = "e capturado los datos correctamente"
+  	#	redirect_to action: 'new'
+  	#else
+  	#	flash[:notice] = " no e capturado los datos correctamente"
+  	#	render :new
+  	#end
+  end
+ end
--- Turismo/test/models/user_test.rb
+++ Turismo/test/models/user_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class InterestTest < ActiveSupport::TestCase
+class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
